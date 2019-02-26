272f0688cca1763530630212678dab778a811456
Taylor Otwell <taylor@laravel.com>
2019-02-26 09:48:38 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
  * Set the default documentation version...
  */
 if (! defined('DEFAULT_VERSION')) {
-    define('DEFAULT_VERSION', '5.7');
+    define('DEFAULT_VERSION', '5.8');
 }
 
 Route::get('/', function () {
